a10ed26dd73a4ce7c207f29b2c6522465987d6f9
ACHM82 <alvaroc>
2022-04-19 07:24:01 +0200

    First design blocks

--- initBuses.m
+++ initBuses.m
@@ -5,7 +5,7 @@ envFolder = strcat(currentFolder,'/Environment');
 designFolder = strcat(currentFolder,'/Design');
 % Loading environment buses
 ACE9C_ACE8x_9C = strcat(envFolder,'ACE9C_ACE8x_9C.mat');
-ACE11C_ACE8x_11C = strcat(envFolder,'ACE11C_ACE8x_11C.mat');
+ACE11C_ACE10x_11C = strcat(envFolder,'ACE11C_ACE10x_11C.mat');
 FCC1_ACExxxA_C1_A = strcat(envFolder,'FCC1_ACExxxA_C1_A.mat');
 FCC2_ACExxxA_M_A = strcat(envFolder,'FCC2_ACExxxA_M_A.mat');
 FCC1_SEMACEx_OUTER_C1 = strcat(envFolder,'FCC1_SEMACEx_OUTER_C1.mat');
